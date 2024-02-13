c32883443e3dfd656f202f1bb5d49770602afb80
Samuel Bartík <63553146+sambartik@users.noreply.github.com>
2024-02-13 16:59:33 +0100

    Change the validators to conform to the protocol spec. requirements

--- shared/validators.py
+++ shared/validators.py
@@ -4,4 +4,4 @@ def valid_username(username: str) -> bool:
   return (3 <= len(username) <= 12) and re.fullmatch(r'[a-zA-Z0-9]+', username)
 
 def valid_message(message: str) -> bool:
-  return 1 <= len(message) <= 1024
+  return 1 <= len(message) <= 1000
